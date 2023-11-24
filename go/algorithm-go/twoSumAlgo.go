c4846b8edea736fe155799486399f83066707df0
JK-Kim4 <jongbell4@gmail.com>
2023-11-24 14:26:02 +0900

    algo step 01 - two sum

---
+++ go/algorithm-go/twoSumAlgo.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+
+func twoSum(intArr []int, target int) (bool) {
+
+
+}
+
+
+func main(){
+
+	fmt.Println(twoSum([]int{1,5,2,7,8,10}, 14))
+	fmt.Println(twoSum([]int{3,5,3,6,77,2}, 21))
+
+
+}
