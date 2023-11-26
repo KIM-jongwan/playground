7562cc6d998b4795dc2e9ea2b1b853e001380060
jongwan <jongbell4@gmail.com>
2023-11-26 23:38:09 +0900

    linkedList 기본구조

--- go/algorithm-go/step-03/LinkedList.go
+++ go/algorithm-go/step-03/LinkedList.go
@@ -5,28 +5,22 @@ Linked List 구현
 */
 
 type List interface {
-	//get(idx int)
-	get(idx int) (data Node)
-	//insert_back(data object)
-	insert_back(data Node)
-	//insert_front(data object)
-	insert_front(data Node)
-	//insert_at(data object, idx int)
-	insert_at(data Node, idx int)
-	//remove_back() (data object)
-	remove_back() (data Node)
-	//remove_front() (data object)
-	remove_front() (data Node)
-	//remove_at(idx int) (data object)
-	remove_at(idx int) (data Node)
+ get(idx int) (data Node)
+ insert_back(data Node)
+ insert_front(data Node)
+ insert_at(data Node, idx int)
+ remove_back() (data Node)
+ remove_front() (data Node)
+ remove_at(idx int) (data Node)
 }
 
 type Node struct {
- data string
-	next *Node
+ data interface{}
+ next *Node
 }
 type LinkedList struct {
-
+ head Node
+	tail Node
 }
 
 func main(){
