63476e948b1e45c94f75c7b35c884ca49ea44487
JK-Kim4 <jongbell4@gmail.com>
2023-11-27 15:41:24 +0900

    linked list 구현

--- go/algorithm-go/step-03/LinkedList.go
+++ go/algorithm-go/step-03/LinkedList.go
@@ -1,28 +1,42 @@
 package main
 
+import "fmt"
+
 /*
 Linked List 구현
 */
 
 type List interface {
- get(idx int) (data Node)
- insert_back(data Node)
- insert_front(data Node)
- insert_at(data Node, idx int)
- remove_back() (data Node)
- remove_front() (data Node)
- remove_at(idx int) (data Node)
+	get(idx int) (data *Node)
+	insert(data Node)
+	insert_at(data Node, idx int)
+	remove() (data Node)
+	remove_at(idx int) (data Node)
 }
 
 type Node struct {
- data interface{}
- next *Node
+	Data string //데이터
+	Next *Node  //다음 노드 주소
 }
 type LinkedList struct {
- head Node
-	tail Node
+	Head  *Node //List Head Node 주소
+	Tail  *Node //List Tail Node 주소
+	Count int   //List Count
 }
 
-func main(){
+func (list *LinkedList) get(idx int) *Node {
+
+	if list.Count == 0 {
+		fmt.Println("empty list")
+		return nil
+	}
+
+	current := list.Tail
+	fmt.Println(current.Next)
+
+	return nil
+}
+
+func main() {
 
 }
