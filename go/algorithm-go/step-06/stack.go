b9aa21474e4ea90b32a2f82bf1acaa14ee1aebfe
jongwan <jongbell4@gmail.com>
2023-12-07 20:49:42 +0900

    queue

---
+++ go/algorithm-go/step-06/stack.go
@@ -0,0 +1,32 @@
+package main
+
+type Node struct {
+	Front *Node
+	Rear *Node
+	Data int
+}
+
+type Queue interface{
+	initQueue()
+	append(data int)
+	pop() int
+}
+
+func initQueue(){
+
+}
+
+func (node *Node) append(data int){
+
+}
+
+func pop() (int){
+
+	return 0;
+}
+
+
+func main() {
+
+}
+
