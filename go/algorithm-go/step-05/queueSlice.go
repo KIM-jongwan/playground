d7b5eefc5d099290093d6790087fcfd99d646c84
jongwankim <jongbell4@gmail.com>
2023-12-10 23:13:46 +0900

    pointer 역참조:

--- go/algorithm-go/step-05/queueSlice.go
+++ go/algorithm-go/step-05/queueSlice.go
@@ -1,24 +1,38 @@
 package main
 
+import "fmt"
+
 /*
 	go slice
 	사용하여 queue 구현
 */
 
-type Queue[] interface{
-	EnQueue(data interface{})
-	DeQueue() (data interface{})
+type Queue []interface{
 }
 
-func (q *Queue) EnQueue(data interface{}){
-
+func (q *Queue) IsEmpty() bool {
+	return len(*q) == 0
 }
 
-func (q *Queue) DeQueue() (data interface{}){
-	
+func (q *Queue) Enqueue(data interface{}){
+	*q = append(*q, data);
+	fmt.Println("append queue")
 }
+
 
 
 func main()  {
-	
+
+	var a int = 3;
+
+	q := Queue{}
+	pq := &q
+
+	q.Enqueue(1)
+
+	fmt.Println(&a)
+	fmt.Println(q)
+	fmt.Println(&q)
+	fmt.Println(pq)
+	fmt.Println(*pq)
 }
